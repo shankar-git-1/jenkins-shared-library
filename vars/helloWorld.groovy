656fc0af7ccd51420aab9b1d6282bde84c35f9d3
shankar-git-1 <nanushankar64@gmail.com>
2025-03-21 09:32:59 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,6 @@
 // vars/helloWorld.groovy
 def call(Map config = [:]) {
-    sh "echo Hello ${config.name}! Welcome to Jenkins Shared Library. Today is ${config.dayOfWeek}."
+    sh "echo Hello ${config.name}! Welcome to Jenkins Shared Library."
+    sh "echo Today is ${config.dayOfWeek}."
     
 }
